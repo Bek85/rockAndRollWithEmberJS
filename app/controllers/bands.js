020b911100761d113a2421749b7b22bbd674b54a
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-12-24 18:46:30 +0500

    insert router as service to transition to bands.band.songs router after adding a new band name

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,12 +1,15 @@
 import Controller from "@ember/controller";
 import Band from "../models/band";
 import { empty } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: "",
 
   isAddButtonDisabled: empty("newBandName"),
+
+  router: service(),
 
   actions: {
     addBand() {
@@ -22,7 +25,12 @@ export default Controller.extend({
       evt.preventDefault();
       let newBand = Band.create({ name: this.newBandName });
       this.model.pushObject(newBand);
-      this.set("newBandName", "");
+      // this.set("newBandName", "");
+      this.setProperties({
+        newBandName: "",
+        isAddingBand: false,
+      });
+      this.router.transitionTo("bands.band.songs", newBand);
     },
   },
 });
