542fc34dcf07b601e2725b74e296f4f4f8d32be1
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-12-24 12:12:12 +0500

    prevent creating bands without a name

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,9 +1,12 @@
 import Controller from "@ember/controller";
 import Band from "../models/band";
+import { empty } from "@ember/object/computed";
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: "",
+
+  isAddButtonDisabled: empty("newBandName"),
 
   actions: {
     addBand() {
@@ -14,10 +17,12 @@ export default Controller.extend({
       this.set("isAddingBand", false);
     },
 
-    saveBand() {
+    saveBand(evt) {
       // Create a new band
+      evt.preventDefault();
       let newBand = Band.create({ name: this.newBandName });
       this.model.pushObject(newBand);
+      this.set("newBandName", "");
     },
   },
 });
