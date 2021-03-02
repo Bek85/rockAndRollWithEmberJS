eac677630cd57b5e4a0f86a44fc6aa00c28f09f6
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-03 00:31:22 +0500

    add ember-cli-sass & convert current style files to .scss extension

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
+const nodeSass = require("node-sass");
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    sassOptions: {
+      implementation: nodeSass,
+    },
     // Add options here
   });
 
