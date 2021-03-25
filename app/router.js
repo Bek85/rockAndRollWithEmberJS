99b443006f9bd275803ec0d47eebdbca540a11fe
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-25 18:35:26 +0500

    create setDocumentTitle function to append descriptive titles to browser tab titles

--- app/router.js
+++ app/router.js
@@ -1,9 +1,48 @@
 import EmberRouter from "@ember/routing/router";
 import config from "./config/environment";
+import { capitalize } from "@ember/string";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
+
+  init() {
+    this._super();
+    this.setDocumentTitle();
+  }
+
+  setDocumentTitle() {
+    this.on("routeDidChange", (transition) => {
+      if (!transition.to) {
+        return;
+      }
+
+      let toRouteName = transition.to.name;
+      let pageTitles = {
+        "bands.index": () => {
+          return "Bands";
+        },
+        "bands.band.songs": () => {
+          let bandRouteInfo = transition.to.find((info) =>
+            info.name.includes("bands.band")
+          );
+          let bandSlug = bandRouteInfo.params.slug;
+          let bandName = bandSlug
+            .split("-")
+            .map((s) => capitalize(s))
+            .join("");
+          return `${bandName} songs`;
+        },
+      };
+      let titleSegments = [];
+      let titleSetter = pageTitles[toRouteName];
+      if (titleSetter) {
+        titleSegments.push(titleSetter());
+      }
+      titleSegments.push("Rock and Roll with Ember.js");
+      document.title = titleSegments.join(" - ");
+    });
+  }
 }
 
 Router.map(function () {
@@ -15,7 +54,7 @@ Router.map(function () {
       },
       function () {
         this.route("songs");
-        this.route('details');
+        this.route("details");
       }
     );
   });
