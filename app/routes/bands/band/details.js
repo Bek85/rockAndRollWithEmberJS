d05af203c8747022e5b97af482fe2579403726f1
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-25 23:14:31 +0500

    use routeWillChange event to warn a user when transition to another route is about to happen

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,6 +1,24 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default class DetailsRoute extends Route {
+  @service router;
+
+  init() {
+    this._super();
+    this.router.on("routeWillChange", (transition) => {
+      if (!transition.from) return;
+      console.log(transition.to.name, transition.from.name);
+      if (transition.to.name === transition.from.name) return;
+
+      let controller = this.controller;
+      if (controller && controller.isEditing) {
+        let leave = window.confirm("Are you sure?");
+        !leave ? transition.abort() : controller.set("isEditing", false);
+      }
+    });
+  }
+
   model() {
     return this.modelFor("bands.band");
   }
