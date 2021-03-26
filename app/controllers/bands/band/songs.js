e3bd8a8086330519df247691cd05e51dd9eacd1f
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-26 23:04:01 +0500

    use "on" modifier to bind events in songs template & refactor songs controller to es6 class syntax

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -2,37 +2,39 @@ import Controller from "@ember/controller";
 import { empty } from "@ember/object/computed";
 import { action } from "@ember/object";
 
-export default Controller.extend({
-  isAddingSong: false,
-  newSongTitle: "",
+export default class SongsController extends Controller {
+  isAddingSong = false;
+  newSongTitle = "";
 
-  isAddButtonDisabled: empty("newSongTitle"),
+  @empty("newSongTitle") isAddButtonDisabled;
 
-  actions: {
-    addSong() {
-      this.set("isAddingSong", true);
-    },
+  @action
+  addSong() {
+    this.set("isAddingSong", true);
+  }
 
-    cancelAddSong() {
-      this.set("isAddingSong", false);
-    },
+  @action
+  cancelAddSong() {
+    this.set("isAddingSong", false);
+  }
 
-    async saveSong(evt) {
-      evt.preventDefault();
-      // let newSong = Song.create({ title: this.newSongTitle });
-      // this.model.songs.pushObject(newSong);
-      let newSong = this.store.createRecord("song", {
-        title: this.get("newSongTitle"),
-        band: this.model,
-      });
-      await newSong.save();
-      this.set("newSongTitle", "");
-      // console.log(this.model);
-    },
-  },
+  @action
+  async saveSong(evt) {
+    evt.preventDefault();
+    // let newSong = Song.create({ title: this.newSongTitle });
+    // this.model.songs.pushObject(newSong);
+    let newSong = this.store.createRecord("song", {
+      title: this.get("newSongTitle"),
+      band: this.model,
+    });
+    await newSong.save();
+    this.set("newSongTitle", "");
+    // console.log(this.model);
+  }
 
-  updateRating: action(function (song, rating) {
+  @action
+  updateRating(song, rating) {
     song.set("rating", song.rating === rating ? 0 : rating);
     song.save();
-  }),
-});
+  }
+}
